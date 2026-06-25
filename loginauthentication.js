042c4dd15c81744582af4def6b0e3ec266ac5a60
nk960290-ui <nk960290@gmail.com>
2026-06-25 17:50:31 +0530

    Add files via upload

---
+++ loginauthentication.js
@@ -0,0 +1,12 @@
+let username = "nandha"
+let password = "1234";
+
+let enteredusername ="nandha"
+let enteredpassword ="1234"
+
+let result = (username===enteredusername
+    && password===enteredpassword) 
+    ?"Login Successful"
+    :"invalid credentials";
+
+console.log(result);
