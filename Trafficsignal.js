bfd7099ed79b60d68d49bf6621327d01358a6492
nk960290-ui <nk960290@gmail.com>
2026-06-25 17:48:10 +0530

    Add files via upload

---
+++ Trafficsignal.js
@@ -0,0 +1,18 @@
+let signal ="red";
+
+switch(signal) {
+    case "red":
+        console.log("stop");
+        break;
+
+    case "yellow":
+        console.log(" ready");
+        break;
+
+    case "green":
+        console.log("go");
+        break;
+        
+    default:
+        console.log("Invalid signal");
+}
